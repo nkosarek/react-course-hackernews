21c35f70d8072869948235ebb8652242b1f3fd7f
Nicholas Kosarek <nkosarek379@gmail.com>
2017-10-17 16:01:38 -0700

    Controlled Components

--- src/App.js
+++ src/App.js
@@ -63,6 +63,7 @@ class App extends Component {
         <form>
           <input
             type="text"
+            value={searchTerm}
             onChange={this.onSearchChange}
           />
         </form>
