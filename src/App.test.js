91c076ef5dc5eb3f41a0fa2fe19421f63d5aba49
Nicholas Kosarek <nkosarek379@gmail.com>
2017-11-09 14:40:37 -0800

    Unit Tests with Enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App, { Search, Button, Table } from './App';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('App', () => {
 
@@ -64,6 +68,14 @@ describe('Table', () => {
   it('renders', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Table { ...props } />, div);
+  });
+
+  it('shows two items in list', () => {
+    const element = shallow(
+      <Table { ...props } />
+    );
+
+    expect(element.find('.table-row').length).toBe(2);
   });
 
   test('snapshots', () => {
